e126e49775280a1037b8fb38f70f7979b8ad8977
Keith <krush@lostminions.org>
2016-03-17 12:10:15 -0700

    my code

---
+++ Assets/scripts/Bocce Ball/HUD/TeamScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeamScore : MonoBehaviour
+{
+    public void SetColor(Color color)
+    {
+        GetComponent<GUIText>().color = color;
+    }
+
+    public void SetScore(int score)
+    {
+        GetComponent<GUIText>().text = score.ToString();
+    }
+}
